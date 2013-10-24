c62b03ff2561c0058a6819c641476c89ef117fc7
Titus von der Malsburg <malsburg@gmail.com>
2013-10-24 11:17:27 +0200

    Initial version

---
+++ helm-dictionary.el
@@ -0,0 +1,155 @@
+;;; helm-dictionary.el --- Helm source for looking up dictionaries
+
+;; Copyright (C) 2013 Titus von der Malsburg <malsburg@posteo.de>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Install:
+
+;; Put this file on your Emacs-Lisp load path and add the following in
+;; your Emacs startup file:
+;;
+;;     (require 'helm-dictionary)
+;;
+;; Alternatively, you can use autoload:
+;;
+;;     (autoload 'helm-dictionary "helm-dictionary" "" t)
+;;
+;; In order to specify a dictionary set the variable
+;; `helm-dictionary-database' to the filename of that dictionary.
+;;
+;; A dictionary for German and English can be found in the Debian
+;; package trans-de-en.  This package is also available in many
+;; distributions derived from Debian such as Ubuntu.  Alternatively,
+;; this dictionary can also be downloaded here:
+;; 
+;;   http://www-user.tu-chemnitz.de/~fri/ding/
+;;
+;; A dictionary for German and Spanish can be found here:
+;;
+;;   https://savannah.nongnu.org/projects/ding-es-de
+;;
+;; A variety of dictionaries with English as the source or target
+;; language can be found here:
+;;
+;;   https://en.wiktionary.org/wiki/User:Matthias_Buchmeier
+;;
+;; These dictionaries were automatically created from the Wiktionary
+;; database.  Their size and quality may vary.  Also generated from
+;; Wiktionary are the following dictionaries with Russian as the
+;; source or target language:
+;;
+;;   http://wiktionary-export.nataraj.su/en/
+;;
+
+;;; Code:
+
+(eval-when-compile (require 'cl))
+(require 'helm)
+
+(defgroup helm-dictionary nil
+  "Helm plugin for looking up a dictionary."
+  :group 'helm)
+
+(defcustom helm-dictionary-database "/usr/share/trans/de-en"
+  "The file containing the dictionary."
+  :group 'helm-dictionary
+  :type  'file)
+
+
+(defun helm-dictionary-init ()
+  "Initialize async grep process for `helm-source-dictionary'."
+  (let ((process-connection-type nil)
+        (cmd (format "grep -i '%s' %s | grep -v '^#'"
+                     helm-pattern
+                     helm-dictionary-database)))
+    (prog1
+      (start-process-shell-command "helm-dictionary" helm-buffer cmd)
+      (set-process-sentinel
+        (get-buffer-process helm-buffer)
+        #'(lambda (process event)
+            (if (string= event "finished\n")
+                (with-helm-window
+                  (setq mode-line-format
+                        '(" " mode-line-buffer-identification " "
+                          (line-number-mode "%l") " "
+                          (:eval (propertize
+                                  (format "[Grep Process Finish- (%s results)]"
+                                          (max (1- (count-lines
+                                                    (point-min) (point-max))) 0))
+                                  'face 'helm-grep-finish))))
+                  (force-mode-line-update))
+                (helm-log "Error: Egrep %s"
+                          (replace-regexp-in-string "\n" "" event))))))))
+
+
+(defun helm-dictionary-transformer (candidates)
+  "Formats entries retrieved from the data base."
+  (loop for i in candidates
+        for entry = (split-string i " :: ")
+        for l1terms = (split-string (car entry) " | ")
+        for l2terms = (split-string (cadr entry) " | ")
+        for width = (save-excursion (with-helm-window (window-width)))
+        append
+        (loop for l1term in l1terms
+              for l2term in l2terms
+              if (or (string-match helm-pattern l1term)
+                     (string-match helm-pattern l2term))
+              collect
+              (cons 
+                (concat
+                  (truncate-string-to-width l1term (- (/ width 2) 1) 0 ?\s)
+                  " "
+                  (truncate-string-to-width l2term (- (/ width 2) 1) 0 ?\s))
+                (cons l1term l2term)))))
+
+
+(defun helm-dictionary-insert-l1term (entry)
+  (insert
+    (replace-regexp-in-string
+      " *{.+}\\| *\\[.+\\]" "" (car entry))))
+
+(defun helm-dictionary-insert-l2term (entry)
+  (insert
+    (replace-regexp-in-string
+      " *{.+}\\| *\\[.+\\]" "" (cdr entry))))
+
+
+(defvar helm-source-dictionary
+  '((name . "Search dictionary")
+    (candidates-process . helm-dictionary-init)
+    (candidate-transformer . helm-dictionary-transformer)
+    (delayed)
+    (nohighlight)
+    (no-matchplugin)
+    (action . (("Insert German term"  . helm-dictionary-insert-l1term)
+               ("Insert English term" . helm-dictionary-insert-l2term)))))
+
+;;;###autoload
+(defun helm-dictionary ()
+  (interactive)
+  (helm :sources 'helm-source-dictionary
+        :full-frame t
+        :candidate-number-limit 500
+        :buffer "*helm dictionary*"))
+
+(provide 'helm-dictionary)
+
+;; Local Variables:
+;; byte-compile-warnings: (not cl-functions obsolete)
+;; coding: utf-8
+;; indent-tabs-mode: nil
+;; End:
+
+;;; helm-dictionary.el ends here
