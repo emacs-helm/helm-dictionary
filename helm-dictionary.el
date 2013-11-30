98a17329e9a3e6a18e692e1799deef7f71813fae
Titus von der Malsburg <malsburg@gmail.com>
2013-11-30 13:52:10 +0100

    Fixed type of default setting for helm-dictionary-browser-function.
    Also replaced tabs by spaces for consistency with the rest of the code.

--- helm-dictionary.el
+++ helm-dictionary.el
@@ -101,39 +101,39 @@ the browser specified by the customization variable
 values that are admissible for the `browse-url-browser-function'."
   :group 'helm-dictionary
   :type '(choice
-	  (function-item :tag "Currently configured default for `browse-url'"
+          (const         :tag "Currently configured default for `browse-url'"
                          :value nil)
-	  (function-item :tag "Emacs W3" :value  browse-url-w3)
-	  (function-item :tag "W3 in another Emacs via `gnudoit'"
-			 :value  browse-url-w3-gnudoit)
-	  (function-item :tag "Mozilla" :value  browse-url-mozilla)
-	  (function-item :tag "Firefox" :value browse-url-firefox)
-	  (function-item :tag "Chromium" :value browse-url-chromium)
-	  (function-item :tag "Galeon" :value  browse-url-galeon)
-	  (function-item :tag "Epiphany" :value  browse-url-epiphany)
-	  (function-item :tag "Netscape" :value  browse-url-netscape)
-	  (function-item :tag "eww" :value  eww-browse-url)
-	  (function-item :tag "Mosaic" :value  browse-url-mosaic)
-	  (function-item :tag "Mosaic using CCI" :value  browse-url-cci)
-	  (function-item :tag "Text browser in an xterm window"
-			 :value browse-url-text-xterm)
-	  (function-item :tag "Text browser in an Emacs window"
-			 :value browse-url-text-emacs)
-	  (function-item :tag "KDE" :value browse-url-kde)
-	  (function-item :tag "Elinks" :value browse-url-elinks)
-	  (function-item :tag "Specified by `Browse Url Generic Program'"
-			 :value browse-url-generic)
-	  (function-item :tag "Default Windows browser"
-			 :value browse-url-default-windows-browser)
-	  (function-item :tag "Default Mac OS X browser"
-			 :value browse-url-default-macosx-browser)
-	  (function-item :tag "GNOME invoking Mozilla"
-			 :value browse-url-gnome-moz)
-	  (function-item :tag "Default browser"
-			 :value browse-url-default-browser)
-	  (function :tag "Your own function")
-	  (alist :tag "Regexp/function association list"
-		 :key-type regexp :value-type function))
+          (function-item :tag "Emacs W3" :value  browse-url-w3)
+          (function-item :tag "W3 in another Emacs via `gnudoit'"
+                         :value  browse-url-w3-gnudoit)
+          (function-item :tag "Mozilla" :value  browse-url-mozilla)
+          (function-item :tag "Firefox" :value browse-url-firefox)
+          (function-item :tag "Chromium" :value browse-url-chromium)
+          (function-item :tag "Galeon" :value  browse-url-galeon)
+          (function-item :tag "Epiphany" :value  browse-url-epiphany)
+          (function-item :tag "Netscape" :value  browse-url-netscape)
+          (function-item :tag "eww" :value  eww-browse-url)
+          (function-item :tag "Mosaic" :value  browse-url-mosaic)
+          (function-item :tag "Mosaic using CCI" :value  browse-url-cci)
+          (function-item :tag "Text browser in an xterm window"
+                         :value browse-url-text-xterm)
+          (function-item :tag "Text browser in an Emacs window"
+                         :value browse-url-text-emacs)
+          (function-item :tag "KDE" :value browse-url-kde)
+          (function-item :tag "Elinks" :value browse-url-elinks)
+          (function-item :tag "Specified by `Browse Url Generic Program'"
+                         :value browse-url-generic)
+          (function-item :tag "Default Windows browser"
+                         :value browse-url-default-windows-browser)
+          (function-item :tag "Default Mac OS X browser"
+                         :value browse-url-default-macosx-browser)
+          (function-item :tag "GNOME invoking Mozilla"
+                         :value browse-url-gnome-moz)
+          (function-item :tag "Default browser"
+                         :value browse-url-default-browser)
+          (function      :tag "Your own function")
+          (alist         :tag "Regexp/function association list"
+                         :key-type regexp :value-type function))
 )
 
 
